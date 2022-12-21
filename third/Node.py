e55684a01f17785583c51955e442ca43575d3b78
e2380269 <e2380269@ceng.metu.edu.tr>
2022-12-21 15:38:29 +0300

    arly version of belmon ford

--- third/Node.py
+++ third/Node.py
@@ -1,45 +1,92 @@
 import sys
+import socket
+import threading
 INF = sys.maxsize
+HOST = "127.0.0.1"
 
 class Edge:
-    fromNode = None
     toNode = None
-    Cost = INF
+    cost = INF
 
     def printEdge(self):
-        print(f"{self.fromNode} -{self.toNode} | {self.Cost}")
+        print(f" -{self.toNode} | {self.cost}")
 
 class Node:
     nodeId = None
     edgesList = []
 
+    def __init__(self,nodeId):
+        self.nodeId = nodeId
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((HOST,nodeId))
+        self.s.listen()
+        self.s.settimeout(5) 
+
+    def updateEdge(self,u_nodeId,u_cost):
+        for edge in self.edgesList:
+            if(edge.toNode == u_nodeId):
+                edge.cost = u_cost
+
     def printNode(self):
         for edge in self.edgesList:
+            print(f"{self.nodeId}", end='')
             edge.printEdge()
 
 
-nodes = []
-nodeId = sys.argv[1]
+
+read_nodeId = sys.argv[1]
 fd = open(f"{sys.argv[1]}.costs", "r")
 lineCount = 0
 nodeCount = 0
-tempNode = Node()
+tempNode = Node(int(read_nodeId))
 
-tempNode.nodeID = int(nodeId)
+def transmit_distance_vector():
+    for edge in tempNode.edgesList:
+        destination = edge.toNode
+        edge_s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            edge_s.connect((HOST, destination))
+        except ConnectionRefusedError:
+            continue
+
+        edge_s.sendall(bytes(str(tempNode.nodeId), 'utf-8'))
+        edge_s.close()
+
+def receive_distance_vector():
+    tempNode.s.settimeout(6)
+    while True:
+        try:
+            conn, addr = tempNode.s.accept()
+            data = conn.recv(2048)
+            print(data)
+        except socket.timeout:
+            tempNode.printNode()
+            tempNode.s.close()
+            break
+    
+
 lines = fd.readlines()
-
-
 for line in lines:
     if(lineCount == 0):
         nodeCount = int(line)
-        print(nodeCount)
+        for i in range(nodeCount):
+            tempEdge = Edge()
+            tempEdge.toNode = 3000+i
+            if(3000+i == tempNode.nodeId):
+                tempEdge.cost = 0
+            tempNode.edgesList.append(tempEdge)
     else:
         lineElements = line.split(' ')
         tempEdge = Edge()
-        tempEdge.fromNode = int(nodeId)
-        tempEdge.toNode = int(lineElements[0])
-        tempEdge.Cost = int(lineElements[1])
-        tempNode.edgesList.append(tempEdge)
+        destination = int(lineElements[0])
+        distance = int(lineElements[1])
+        tempNode.updateEdge(destination,distance)
     lineCount += 1
-nodes.append(tempNode)
-tempNode.printNode()
+
+transmitThread = threading.Thread(target=transmit_distance_vector, daemon=True)
+transmitThread.start()
+transmitThread.join()
+
+receiveThread = threading.Thread(target=receive_distance_vector, daemon=True)
+receiveThread.start()
+receiveThread.join()
